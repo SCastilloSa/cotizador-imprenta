4b88854f8ad760ddc8d28ec37c630ffa5b9056fc
SCastilloSa <113778364+SCastilloSa@users.noreply.github.com>
2022-09-26 22:44:39 -0300

    refactor

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+pagoOperacionesMenores = {
+  alzado: true,
+  separado: true,
+  corcheteado: true,
+  corte: true,
+  perforado: true,
+  pagoOperacionesMenores: 15,
+  pagoAlzado: 500,
+  pagoImpresion: 1600,}
 let data = {
     nombre: "sebastian",
     tipoTrabajo: "talonarios",
@@ -14,123 +23,93 @@ let data = {
     hojasResma: 500,
     costoResma: 8000,
     costoTinta: 2,
-    alzado: true,
-    separado: true,
-    corcheteado: true,
-    corte: true,
-    perforado: true,
-    pagoOperacionesMenores: 15,
-    pagoAlzado: 500,
-    pagoImpresion: 1600,
+    pagoOperacionesMenores
 }
 
-const tirajeUtil = (data) => {
-    return 
+const tirajeUtil = ({moldes,cantidad, multiplicidad}) => cantidad / moldes * multiplicidad
+
+
+
+
+
+
+
+costoImpresion: function () {
+    return tirajeReal() * costotinta 
 }
-
-
-
-function calculos (data) {
-    
-    const calculos2 = {
-
-    pagoOperacionesMenores: 15,
-    pagoAlzado: 500,
-    pagoImpresion: 1600,
-    tirajeUtil: function () {
-      return cantidad / moldes * multiplicidad
-    },
-    tirajeReal: function () {
-      return this.tirajeUtil() + sobrantes * multiplicidad
-    },
-    costoPapel: function () {
-      return costoresma / hojasresma * this.tirajeReal()
-    },
-    diseno: function () {
-      return diseno
-    },
-    costoImpresion: function () {
-      return this.tirajeReal() * costotinta
-    },
-    costoManoDeObra: function () {
-      const x = this.tirajeUtil() % 1000
+  costoManoDeObra: function () {
+    const x = tirajeUtil() % 1000
+    if (x === 0) {
+      return pagoImpresion * tirajeUtil() / 1000
+    } else {
+      return pagoImpresion * parseInt(tirajeUtil() / 1000 + 1)
+    } 
+}
+  costoAlzado: function () {
+    if (alzado) {
+      const x = tirajeUtil() % 1000
       if (x === 0) {
-        return this.pagoImpresion * this.tirajeUtil() / 1000
+        return pagoAlzado * tirajeUtil() / 1000
       } else {
-        return this.pagoImpresion * parseInt(this.tirajeUtil() / 1000 + 1)
+        return pagoAlzado * (parseInt(tirajeUtil() / 1000) + 1)
       }
-    },
-    costoAlzado: function () {
-      if (alzado) {
-        const x = this.tirajeUtil() % 1000
-        if (x === 0) {
-          return this.pagoAlzado * this.tirajeUtil() / 1000
-        } else {
-          return this.pagoAlzado * (parseInt(this.tirajeUtil() / 1000) + 1)
-        }
-      } else {
-        return 0
-      }
-    },
-    costoPerforado: function () {
-      if (perforado) {
-        const x = this.tirajeUtil() % 1000
-        if (x === 0) {
-          return this.pagoAlzado * this.tirajeUtil() / 1000
-        } else {
-          return this.pagoAlzado * (parseInt(this.tirajeUtil() / 1000) + 1)
-        }
-      } else {
-        return 0
-      }
-    },
-    costoTerminado: function () {
-      return this.tirajeUtil() / untal * this.pagoOperacionesMenores
-    },
-    costoSeparado: function () {
-      if (separado) {
-        return this.tirajeUtil() / untal * this.pagoOperacionesMenores
-      } else {
-        return 0
-      }
-    },
-    costoCorcheteado: function () {
-      if (corcheteado) {
-        return this.tirajeUtil() / untal * this.pagoOperacionesMenores
-      } else {
-        return 0
-      }
-    },
-    costoCorte: function () {
-      if (corte) {
-        return this.tirajeUtil() / untal * this.pagoOperacionesMenores
-      } else {
-        return 0
-      }
-    },
-    costoOperacional: function () {
-      return diseno + this.costoPapel() + this.costoImpresion() +
-          this.costoManoDeObra() + this.costoAlzado() + this.costoSeparado() +
-          this.costoCorcheteado() + this.costoCorte() + this.costoTerminado() +
-          this.costoPerforado()
-    },
-    costoTotal: function () {
-      return this.costoOperacional() * (1 + gastosGenerales / 100)
-    },
-    precioDeVenta: function () {
-      let precio = this.costoTotal() * (1 + utilidad / 100) + impuesto / 100 * this.costoTotal() * utilidad / 100
-      precio = Math.ceil(precio / 100) * 100
-      return precio
-    },
-    precioConIVA: function () {
-      return this.precioDeVenta() * 1.19
-    }
-  }
-
-  return calculos2
+    } else {
+      return 0
+    } 
 }
-
-
+  costoPerforado: function () {
+    if (perforado) {
+      const x = tirajeUtil() % 1000
+      if (x === 0) {
+        return pagoAlzado * tirajeUtil() / 1000
+      } else {
+        return pagoAlzado * (parseInt(tirajeUtil() / 1000) + 1)
+      }
+    } else {
+      return 0
+    } 
+}
+  costoTerminado: function () {
+    return tirajeUtil() / untal * pagoOperacionesMenores 
+}
+  costoSeparado: function () {
+    if (separado) {
+      return tirajeUtil() / untal * pagoOperacionesMenores
+    } else {
+      return 0
+    } 
+}
+  costoCorcheteado: function () {
+    if (corcheteado) {
+      return tirajeUtil() / untal * pagoOperacionesMenores
+    } else {
+      return 0
+    } 
+}
+  costoCorte: function () {
+    if (corte) {
+      return tirajeUtil() / untal * pagoOperacionesMenores
+    } else {
+      return 0
+    } 
+}
+  costoOperacional: function () {
+    return diseno + costoPapel() + costoImpresion() +
+        costoManoDeObra() + costoAlzado() + costoSeparado() +
+        costoCorcheteado() + costoCorte() + costoTerminado() +
+        costoPerforado() 
+    }
+  costoTotal: function () {
+    return costoOperacional() * (1 + gastosGenerales / 100) 
+}
+  precioDeVenta: function () {
+    let precio = costoTotal() * (1 + utilidad / 100) + impuesto / 100 * costoTotal() * utilidad / 100
+    precio = Math.ceil(precio / 100) * 100
+    return precio 
+}
+  precioConIVA: function () {
+    return precioDeVenta() * 1.19
+  }
 
 function $cl (number) {
   let pesos = new Intl.NumberFormat('cl-ES').format(number)
